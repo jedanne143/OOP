243151aeef23fdd133fe4dcd639ee6a796924921
Jedaiah Tantanella <jedanne143@gmail.com>
2024-08-14 08:37:04 -0400

    Part 4 fixed some bugs

--- app.js
+++ app.js
@@ -24,33 +24,9 @@ console.log(each));
 console.log(`${adventurer.name}\n ${adventurer.health}`);
 console.log(adventurer.roll());
 
-//Part 2 Class Fantasy
-class Character{
-  constructor (name) {
-    this.name = name;
-    this.health = 100;
-    this.inventory = []
-  };
-    roll (mod = 0) {
-      const result = Math.floor(Math.random() * 20) + 1 + mod;
-      console.log(`${this.name} rolled a ${result}.`)
-  
-}
-}
-
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
-
-console.log(robin)  //to check what's in Robin
-
-//Part 3
+//Part 2 with Part3
 class Character {
-  static MAX_HEALTH = 100;
+  static MAX_HEALTH = 100;  //Part4 instruction
   constructor (name) {
     this.name = name;
     this.health = 100;
@@ -62,7 +38,7 @@ class Character {
       }
 }
 class Adventurer extends Character{
-  static ROLES = ["Fighter","Healer", "Wizard", "Screamer"];
+  static ROLES = ["Fighter","Healer", "Wizard", "Destroyer"];
   constructor (name, role, companion) {
     super(name);
     // Adventurers have specialized roles.
@@ -99,17 +75,18 @@ class Adventurer extends Character{
   }
   
 const robinCompanion = new Companion ("Leo", "cat")
-const robin_ = new Adventurer("robin","climber", robinCompanion);
+const robin_ = new Adventurer("Robinhood","climber", robinCompanion);
 const anne = new Adventurer("Anne","coder");
-const anneCompanion=new Companion("planty","plant");
-console.log(robin_.toString());
-console.log(Anne.toString());
+const anneCompanion=new Companion("Plants","Healer");
+
 
 //Part 4
 //Add a static MAX_HEALTH property to the Character class, equal to 100.
 // Add a static ROLES array to the Adventurer class, with the values “Fighter,” “Healer,” and “Wizard.” Feel free to add other roles, if you desire!
 // Add a check to the constructor of the Adventurer class that ensures the given role matches one of these values.
-Adventurer.ROLES.forEach(()=>{
-console.log(each)
+Adventurer.ROLES.forEach((each)=>{
+  console.log(each)
 })
+console.log('MAXHEALTH is:' , Character.MAX_HEALTH)
+
 
